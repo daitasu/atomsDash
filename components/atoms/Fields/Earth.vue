70e4f76b3035b15b7b79cf04e235d16c780e0e93
daitasu <ifctp.dai.monara@gmail.com>
2019-03-08 01:46:30 +0900

    fix Earth

--- components/atoms/Fields/Earth.vue
+++ components/atoms/Fields/Earth.vue
@@ -1,13 +1,8 @@
 <template lang="pug">
   hr
-  //- v-layout(column, justify-center, align-center)
-  //-   div aaaaaaaa
 </template>
 
 <script>
-// import Logo from "~/components/Logo.vue";
-// import VuetifyLogo from "~/components/VuetifyLogo.vue";
-
 export default {
   // components: {
   // }
@@ -21,7 +16,7 @@ hr {
   border-top: 4px #000000 solid;
   background-color: antiquewhite;
   display: block;
-  position: fixed;
-  bottom: 100px;
+  position: absolute;
+  bottom: 0;
 }
 </style>
