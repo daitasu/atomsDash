9ecfade4bd2e2da45a637eb32460921bbe28897c
daitasu <ifctp.dai.monara@gmail.com>
2019-03-08 01:52:12 +0900

    change template lang to pug

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,61 +1,26 @@
-<template>
-  <v-app>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-tile-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title" style="font-weight: 600; font-size: 18px;"/>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar
-      :clipped-left="clipped"
-      fixed
-      app
-      class="light-blue lighten-5"
-    >
-      <v-toolbar-side-icon @click="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-    </v-toolbar>
-    <v-content>
-      <v-container>
-        <nuxt />
-      </v-container>
-    </v-content>
-    <v-footer
-      :fixed="fixed"
-      app
-    >
-      <span>&copy; 2019</span>
-    </v-footer>
-  </v-app>
+<template lang="pug">
+  v-app
+    v-navigation-drawer(v-model="drawer" :mini-variant="miniVariant" :clipped="clipped" fixed app)
+      v-list
+        v-list-tile(v-for="(item, i) in items" :key="i" :to="item.to" router exact)
+          v-list-tile-action
+            v-icon {{ item.icon }}
+          v-list-tile-content
+            v-list-tile-title(v-text="item.title" style="font-weight: 600; font-size: 18px;")
+    v-toolbar(:clipped-left="clipped" fixed app class="light-blue lighten-5")
+      v-toolbar-side-icon(@click="drawer = !drawer")
+      v-btn(icon @click.stop="clipped = !clipped")
+        v-icon web
+      v-toolbar-title(v-text="title")
+      v-spacer
+    v-content
+      v-container
+        nuxt
+    v-footer(:fixed="fixed" app)
+      span &copy; 2019
 </template>
 
 <script>
-// import colors from "vuetify/es5/util/colors";
 export default {
   created() {
     console.log(this);
