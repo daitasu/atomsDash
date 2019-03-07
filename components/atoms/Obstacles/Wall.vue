0cd92c59d2ff804e72fd83389a484fd7aa746d0e
daitasu <ifctp.dai.monara@gmail.com>
2019-03-08 02:12:27 +0900

    add: obstacles directory and Wall component

---
+++ components/atoms/Obstacles/Wall.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+  section
+    div.wall.approaching(:style="`width: ${width}; height: ${height};`")
+</template>
+
+<script>
+export default {
+  props: {
+    width: { type: String, default: "50px" },
+    height: { type: String, default: "80px" }
+  }
+};
+</script>
+
+<style lang="scss">
+.wall {
+  background-color: #555;
+  position: absolute;
+  bottom: 100px;
+  right: -5%;
+}
+.approaching {
+  animation-name: approach;
+  animation-duration: 7s;
+  animation-timing-function: ease-out;
+  animation-delay: 0s;
+  animation-iteration-count: 5;
+  animation-direction: normal;
+  animation-fill-mode: none;
+  animation-play-state: running;
+}
+
+@keyframes approach {
+  0% {
+    right: -5%;
+  }
+  100% {
+    right: 105%;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,19 @@
   v-layout.hoge(column, justify-center, align-center)
     Ball
     Earth
+    Wall
 </template>
 
 <script>
 import Earth from "~/components/atoms/Fields/Earth.vue";
 import Ball from "~/components/atoms/Charactors/Ball.vue";
+import Wall from "~/components/atoms/Obstacles/Wall.vue";
 
 export default {
   components: {
     Earth,
-    Ball
+    Ball,
+    Wall
   }
 };
 </script>
