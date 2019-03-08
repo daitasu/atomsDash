0e468c8a91c2f215228b25ca02cd723b5a5e3fb1
daitasu <ifctp.dai.monara@gmail.com>
2019-03-09 03:44:49 +0900

    update: Move the main page to templates component

---
+++ components/templates/Game/index.vue
@@ -0,0 +1,46 @@
+<template lang="pug">
+  v-layout.hoge(column, justify-center, align-center)
+    section
+      StartDialog(:dialog="showStart" :onOk="play")
+    Ball
+    Earth
+    Wall(:playing="playing" ref="wallPosition")
+</template>
+
+<script>
+import StartDialog from "~/components/molecules/Dialogs/StartDialog";
+import Earth from "~/components/atoms/Fields/Earth";
+import Ball from "~/components/atoms/Charactors/Ball";
+import Wall from "~/components/atoms/Obstacles/Wall";
+
+export default {
+  components: {
+    StartDialog,
+    Ball,
+    Wall,
+    Earth
+  },
+  data() {
+    return {
+      playing: false,
+      showStart: true,
+      obstacleX: null,
+      charactorX: null
+    };
+  },
+  methods: {
+    play() {
+      this.playing = true;
+      this.showStart = false;
+    },
+    judge() {
+      console.log("obstacleX ->", this.obstacleX);
+    }
+  },
+  watch: {
+    playing: function() {
+      console.log(this.$refs);
+    }
+  }
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,14 @@
 <template lang="pug">
-  v-layout.hoge(column, justify-center, align-center)
-    Ball
-    Earth
-    Wall
+  section
+    Template
 </template>
 
 <script>
-import Earth from "~/components/atoms/Fields/Earth.vue";
-import Ball from "~/components/atoms/Charactors/Ball.vue";
-import Wall from "~/components/atoms/Obstacles/Wall.vue";
+import Template from "~/components/templates/Game";
 
 export default {
   components: {
-    Earth,
-    Ball,
-    Wall
+    Template
   }
 };
 </script>
