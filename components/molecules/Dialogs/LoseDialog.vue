1a3225b8d80c516aa4bb4a4c6efb867ff53fc0d7
daitasu <ifctp.dai.monara@gmail.com>
2019-03-09 03:46:24 +0900

    add: the start and finish dialog

---
+++ components/molecules/Dialogs/LoseDialog.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+v-dialog(v-model="dialog" width="370")
+  v-card(style="border-radius: 10px;")
+    v-card-text.text-md-center {{ "GAME OVER" }}
+    v-divider
+    v-card-actions
+      v-spacer.text-md-center
+        v-btn(color="primary" flat="") {{ "RESTART" }}
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialog: false
+    };
+  }
+};
+</script>
---
+++ components/molecules/Dialogs/StartDialog.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+v-dialog(v-model="dialog" width="370")
+  v-card(style="border-radius: 10px;")
+    v-card-text.text-md-center(style="font-weight: 400") {{ "ゲームを始めよう" }}
+    v-divider
+    v-card-actions
+      v-spacer.text-md-center
+        v-btn(color="primary" flat="" @click="onOk()") {{ "START" }}
+</template>
+
+<script>
+export default {
+  props: {
+    dialog: { type: Boolean, default: true },
+    onOk: { type: Function, default: () => {} }
+  }
+};
+</script>
