350b0c892d77f6f92f7cc6ede1d87f89d500f31e
daitasu <ifctp.dai.monara@gmail.com>
2019-03-13 02:39:33 +0900

    add: Lose Dialog

--- components/molecules/Dialogs/LoseDialog.vue
+++ components/molecules/Dialogs/LoseDialog.vue
@@ -1,19 +1,18 @@
 <template lang="pug">
 v-dialog(v-model="dialog" width="370")
   v-card(style="border-radius: 10px;")
-    v-card-text.text-md-center {{ "GAME OVER" }}
+    v-card-text.text-md-center(style="font-weight: 400") {{ "GAME OVER" }}
     v-divider
     v-card-actions
       v-spacer.text-md-center
-        v-btn(color="primary" flat="") {{ "RESTART" }}
+        v-btn(color="primary" flat="" @click="onOk()") {{ "RESTART" }}
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      dialog: false
-    };
+  props: {
+    dialog: { type: Boolean, default: true },
+    onOk: { type: Function, default: () => {} }
   }
 };
 </script>
