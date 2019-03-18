aad62d7ce1e0c6023a34974328e5f170a9fc1d91
daitasu <ifctp.dai.monara@gmail.com>
2019-03-19 00:11:17 +0900

    add: Default atom components

---
+++ components/atoms/Charactors/DefaultAtom.vue
@@ -0,0 +1,171 @@
+<template lang="pug">
+  div.area(:style="`width: ${3 * size}px; height: ${3 * size}px;`")
+    transition(
+      appear
+      v-on:before-appear="customBeforeAppearHookL"
+      v-on:appear="customAppearHook"
+    )
+      div.electron-shell(:style="`width: ${3 * size}px; height: ${3 * size}px;`")
+        div.electron-1(v-if="selectedAtomNo > 6" :class="isOutermostL")
+        div.electron-2(v-if="selectedAtomNo > 7" :class="isOutermostL")
+        div.electron-3(v-if="selectedAtomNo > 8" :class="isOutermostL")
+        div.electron-4(v-if="selectedAtomNo > 9" :class="isOutermostL")
+        transition(
+          appear
+          v-on:before-appear="customBeforeAppearHookL2"
+          v-on:appear="customAppearHook"
+        )
+          div.electron-shell(:style="`width: ${3 * size}px; height: ${3 * size}px; transform: rotateZ(45deg);`")
+            div.electron-1(v-if="selectedAtomNo > 2" :class="isOutermostL")
+            div.electron-2(v-if="selectedAtomNo > 3" :class="isOutermostL")
+            div.electron-3(v-if="selectedAtomNo > 4" :class="isOutermostL")
+            div.electron-4(v-if="selectedAtomNo > 5" :class="isOutermostL")
+    transition(
+      appear
+      v-on:before-appear="customBeforeAppearHookK"
+      v-on:appear="customAppearHook"
+    )
+      div.electron-shell(:style="`width: ${2 * size}px; height: ${2 * size}px;`")
+        div.electron-1(v-if="selectedAtomNo > 0" :class="isOutermostK")
+        div.electron-2(v-if="selectedAtomNo > 1" :class="isOutermostK")
+    div.neutron(:style="`width: ${size}px; height: ${size}px;`")
+</template>
+
+<script>
+import Velocity from "velocity-animate";
+
+export default {
+  props: {
+    size: { type: Number, default: 30 },
+    selectedAtomNo: { type: Number, default: 4 }
+  },
+  computed: {
+    isOutermostK: function() {
+      return this.selectedAtomNo < 3 ? "outermost" : "";
+    },
+    isOutermostL: function() {
+      return this.selectedAtomNo < 11 ? "outermost" : "";
+    }
+  },
+  methods: {
+    customBeforeAppearHookK: function(el) {
+      Velocity(el, { rotateZ: "360deg" }, { duration: 5000, loop: true, easing: "linear" });
+    },
+    customBeforeAppearHookL: function(el) {
+      Velocity(el, { rotateZ: "360deg" }, { duration: 3000, loop: true, easing: "linear" });
+    },
+    customBeforeAppearHookL2: function(el) {
+      Velocity(el, { rotateZ: "90deg" }, { duration: 3000, loop: true, easing: "linear" });
+    },
+    customAppearHook: function(el, done) {
+      done();
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.area {
+  background-color: rgba(255, 193, 77, 0.783);
+  position: absolute;
+  bottom: 100px;
+  left: 150px;
+  z-index: 0;
+}
+.electron-shell {
+  background-color: #fff;
+  border-radius: 50%;
+  border: solid 1px #000;
+  position: absolute;
+  bottom: 0;
+  top: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  z-index: 0;
+}
+.electron-1 {
+  display: inline-block;
+  background-color: #4b90f9;
+  border-radius: 50%;
+  height: 10px;
+  width: 10px;
+  position: absolute;
+  left: -5px;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  z-index: 1;
+}
+.electron-2 {
+  display: inline-block;
+  background-color: #4b90f9;
+  border-radius: 50%;
+  height: 10px;
+  width: 10px;
+  position: absolute;
+  right: -5px;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  z-index: 1;
+}
+.electron-3 {
+  display: inline-block;
+  background-color: #4b90f9;
+  border-radius: 50%;
+  height: 10px;
+  width: 10px;
+  position: absolute;
+  top: -5px;
+  right: 0;
+  left: 0;
+  margin: auto;
+  z-index: 1;
+}
+.electron-4 {
+  display: inline-block;
+  background-color: #4b90f9;
+  border-radius: 50%;
+  height: 10px;
+  width: 10px;
+  position: absolute;
+  bottom: -5px;
+  right: 0;
+  left: 0;
+  margin: auto;
+  z-index: 1;
+}
+.electron-5 {
+  display: inline-block;
+  background-color: #4b90f9;
+  border-radius: 50%;
+  height: 10px;
+  width: 10px;
+  position: absolute;
+  left: -5px;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  z-index: 1;
+}
+.neutron {
+  border-radius: 50%;
+  background-color: #000;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  z-index: 1;
+  font-size: 15px;
+  color: #fff;
+  font-weight: 600;
+  text-align: center;
+  padding: 5%;
+}
+.outermost {
+  background-color: #ff0000f1;
+}
+</style>
--- components/templates/Demo/index.vue
+++ components/templates/Demo/index.vue
@@ -1,19 +1,19 @@
 <template lang="pug">
   v-layout(row, wrap).pl-2
     v-flex(xs12).pt-2
-      Hydrogen
+      Atom
 </template>
 
 <script>
 // import StartDialog from "~/components/molecules/Dialogs/StartDialog";
 // import LoseDialog from "~/components/molecules/Dialogs/LoseDialog";
 // import Earth from "~/components/atoms/Fields/Earth";
-import Hydrogen from "~/components/atoms/Charactors/Hydrogen";
+import Atom from "~/components/atoms/Charactors/DefaultAtom";
 // import Wall from "~/components/atoms/Obstacles/Wall";
 
 export default {
   components: {
-    Hydrogen
+    Atom
   }
 };
 </script>
