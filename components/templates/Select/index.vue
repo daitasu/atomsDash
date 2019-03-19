8ebb68b51da73046892ff58c8d8029fe69de719f
daitasu <ifctp.dai.monara@gmail.com>
2019-03-19 11:10:09 +0900

    add: atom select view

--- components/templates/Select/index.vue
+++ components/templates/Select/index.vue
@@ -1,29 +1,87 @@
 <template lang="pug">
-  v-layout(row, wrap).pl-2
+  v-layout(row wrap style="text-align: center;").pl-2
     v-flex(xs6).pt-2
-      v-layout(row, wrap).pl-2
-        v-flex(xs3).pt-2
-          div s
-        v-flex(xs3).pt-2 
-          div r
-        v-flex(xs3).pt-2
-          div d
-        v-flex(xs3).pt-2
-          div b
+      v-layout(row, wrap).p-1
+        v-flex(xs3).p-2
+          v-card.card H
+            Atom(:size="size" :selectedAtomNo="1" :top="top" :bottom="bottom" :right="right" :left="left")
+        v-flex(xs3).p-2 
+          v-card.hidden
+        v-flex(xs3).p-2
+          v-card.hidden
+        v-flex(xs3).p-2
+          v-card.hidden
     v-flex(xs6).pt-2
-      v-layout(row, wrap).pl-2
-        v-flex(xs3).pt-2
-        v-flex(xs3).pt-2
-        v-flex(xs3).pt-2
-        v-flex(xs3).pt-2
+      v-layout(row, wrap).p-1
+        v-flex(xs3).p-2
+          v-card.hidden
+        v-flex(xs3).p-2 
+          v-card.hidden
+        v-flex(xs3).p-2
+          v-card.hidden
+        v-flex(xs3).p-2
+          v-card.card He
+            Atom(:size="size" :selectedAtomNo="2" :top="top" :bottom="bottom" :right="right" :left="left")
+    v-flex(xs6).pt-2
+      v-layout(row, wrap).p-1
+        v-flex(xs3).p-2
+          v-card.card Li
+            Atom(:size="size" :selectedAtomNo="3" :top="top" :bottom="bottom" :right="right" :left="left")
+        v-flex(xs3).p-2 
+          v-card.card Be
+            Atom(:size="size" :selectedAtomNo="4" :top="top" :bottom="bottom" :right="right" :left="left")
+        v-flex(xs3).p-2
+          v-card.card B
+            Atom(:size="size" :selectedAtomNo="5" :top="top" :bottom="bottom" :right="right" :left="left")
+        v-flex(xs3).p-2
+          v-card.card C
+            Atom(:size="size" :selectedAtomNo="6" :top="top" :bottom="bottom" :right="right" :left="left")
+    v-flex(xs6).pt-2
+      v-layout(row, wrap).p-1
+        v-flex(xs3).p-2
+          v-card.card N
+            Atom(:size="size" :selectedAtomNo="7" :top="top" :bottom="bottom" :right="right" :left="left")
+        v-flex(xs3).p-2 
+          v-card.card O
+            Atom(:size="size" :selectedAtomNo="8" :top="top" :bottom="bottom" :right="right" :left="left")
+        v-flex(xs3).p-2
+          v-card.card F
+            Atom(:size="size" :selectedAtomNo="9" :top="top" :bottom="bottom" :right="right" :left="left")
+        v-flex(xs3).p-2
+          v-card.card Ne
+            Atom(:size="size" :selectedAtomNo="10" :top="top" :bottom="bottom" :right="right" :left="left")
 </template>
 
 <script>
-// import StartDialog from "~/components/molecules/Dialogs/StartDialog";
-// import LoseDialog from "~/components/molecules/Dialogs/LoseDialog";
-// import Earth from "~/components/atoms/Fields/Earth";
-// import Ball from "~/components/atoms/Charactors/Ball";
-// import Wall from "~/components/atoms/Obstacles/Wall";
+import Atom from "~/components/atoms/Charactors/DefaultAtom";
 
-export default {};
+export default {
+  components: {
+    Atom
+  },
+  data() {
+    return {
+      size: 30,
+      top: 35,
+      bottom: 10,
+      left: 0,
+      right: 0
+    };
+  }
+};
 </script>
+
+<style lang="scss" scoped>
+.hidden {
+  display: none;
+}
+.card {
+  height: 125px;
+  font-size: 20px;
+  font-weight: 500;
+
+  &:hover {
+    background-color: #ddd !important;
+  }
+}
+</style>
